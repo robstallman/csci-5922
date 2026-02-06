41914239f2ea8bcffe5fc2053852154facad325b
Rob Stallman <robstallman3@gmail.com>
2026-02-05 17:38:33 -0700

    * Add ability to run on GPU * Add baseline deep network (take 1)

--- Lab2.py
+++ Lab2.py
@@ -9,13 +9,14 @@ import torchvision.transforms.v2 as v2
 from torchvision import datasets
 from torch.utils.data import DataLoader
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # %%
 ############
 ## Part 1 ##
 ############
 
-#%% ----- Selecting a Difficult Dataset -----
+# %% ----- Selecting a Difficult Dataset -----
 
 # Download training and testing sets from CIFAR100
 raw_tfms = v2.Compose([
@@ -36,7 +37,7 @@ test_data = datasets.CIFAR100(
     transform=raw_tfms
 )
 
-# %% ----- Data Preprocessing ----- 
+# %% ----- Data Preprocessing -----
 
 # Concatenate training and testing data along dimension 1 (x axis of each image)
 x_train = torch.concat([sample[0] for sample in training_data], dim=1)
@@ -140,9 +141,19 @@ class TwoLayerNetwork(nn.Module):
         self.linear2.bias.data -= lr * self.linear2.bias.grad
         self.linear3.weight.data -= lr * self.linear3.weight.grad
         self.linear3.bias.data -= lr * self.linear3.bias.grad
-        
+
+
 # Define a function to evaluate a single epoch
-def run_epoch(model: nn.Module, loader: DataLoader, train: bool = True, lr: float = 0.1) -> tuple[torch.tensor, torch.tensor]:
+def run_epoch(
+    model: nn.Module,
+    loader: DataLoader,
+    device: torch.device,
+    train: bool = True,
+    lr: float = 0.1,
+) -> tuple[torch.tensor, torch.tensor]:
+    # Make sure we're on the correct device
+    model = model.to(device)
+
     # Set the model mode – either training or evaluation
     if train:
         model.train()
@@ -157,6 +168,9 @@ def run_epoch(model: nn.Module, loader: DataLoader, train: bool = True, lr: floa
     with context:
         # Iterate through the batches in the loader
         for xb, yb in loader:
+            # Transfer to device
+            xb, yb = xb.to(device), yb.to(device)
+
             # -- Forward pass -- #
             # Get probabilities for each class
             logits = model(xb)
@@ -173,17 +187,33 @@ def run_epoch(model: nn.Module, loader: DataLoader, train: bool = True, lr: floa
             predictions = logits.argmax(dim=1)
             n_correct += (predictions == yb).sum()
             n_total += yb.shape[0]
-    
+
     # Calculate accuracy for this epoch
     acc = n_correct / n_total
 
     # Return loss and accuracy for this epoch
     return loss.item(), acc
 
+
 # Define function for training
-def train_model(model: nn.Module, train_loader: DataLoader, test_loader: DataLoader, lr: int = 0.1, n_epochs: int = 1000, print_every: int = 50, early_stopping: bool = True, patience: int = 5000, min_delta: float = 1e-2):
+def train_model(
+    model: nn.Module,
+    train_loader: DataLoader,
+    test_loader: DataLoader,
+    device: torch.device,
+    lr: int = 0.1,
+    n_epochs: int = 1000,
+    print_every: int = 50,
+    early_stopping: bool = True,
+    patience: int = 5000,
+    min_delta: float = 1e-2,
+):
     # Lists for storing epochs, losses, and accuracies
-    epochs, train_losses, test_losses, train_accuracies, test_accuracies = []
+    epochs = []
+    train_losses = []
+    test_losses = []
+    train_accuracies = []
+    test_accuracies = []
 
     # Early stopping setup
     best_test_loss = float('inf')
@@ -194,10 +224,14 @@ def train_model(model: nn.Module, train_loader: DataLoader, test_loader: DataLoa
     # Start training
     for epoch in range(n_epochs):
         # Training over epoch
-        train_loss, train_acc = run_epoch(model, train_loader, train=True, lr=lr)
+        train_loss, train_acc = run_epoch(
+            model=model, loader=train_loader, device=device, train=True, lr=lr
+        )
 
         # Testing over epoch
-        test_loss, test_acc = run_epoch(model, test_loader, train=False, lr=lr)
+        test_loss, test_acc = run_epoch(
+            model=model, loader=test_loader, device=device, train=False, lr=lr
+        )
 
         # Save losses and accuracies for this epoch
         epochs.append(epoch + 1)
@@ -227,11 +261,11 @@ def train_model(model: nn.Module, train_loader: DataLoader, test_loader: DataLoa
                     print(f"Early stopping triggered at epoch {epoch+1}\n"
                           f"Best epoch was {best_epoch} with test loss {best_test_loss:.4f}")
                     break
-        
+
         # Reload best model
         if early_stopping:
             model.load_state_dict(best_model_weights)
-        
+
         # Gather epochs, losses, accuracies to return
         training_curve = {
             'epochs': epochs,
@@ -242,7 +276,8 @@ def train_model(model: nn.Module, train_loader: DataLoader, test_loader: DataLoa
         }
 
     return model, training_curve
-    
+
+
 # Define a function for visualizing training curves
 def plot_loss_acc(training_curve):
     epochs = training_curve['epochs']
@@ -278,19 +313,156 @@ def save_model(model: nn.Module, name: str):
     # Create a directory for models if it doesn't yet exist
     if not os.path.exists("models"):
         os.mkdir("models")
-    
+
     # Save the model to the directory
     filepath = os.path.join("models", f"{name}.pt")
     torch.save(model.state_dict(), filepath)
 
 # %% ----- Evaluating the Dataset Difficulty: Training -----
 # Create an instance of the model
-model = TwoLayerNetwork()
+model = TwoLayerNetwork().to(device)
 
 # Train the model
-model, training_curve = train_model(model, train_loader, test_loader, n_epochs=6, print_every=2)
+model, training_curve = train_model(
+    model=model,
+    train_loader=train_loader,
+    test_loader=test_loader,
+    device=device,
+    lr=0.1,
+    n_epochs=6,
+    print_every=2,
+)
 
 # Save the trained model
 torch.save(model.state_dict(), "baseline_shallow_model.pt")
 
 plot_loss_acc(training_curve)
+
+
+# %% ----- Building a Baseline Deep Network -----
+
+
+# Define ReLU activation function
+def ReLU(z: torch.tensor) -> torch.tensor:
+    return torch.clamp(z, min=0)
+
+
+# Define tanh activation function
+def tanh(x: torch.tensor) -> torch.tensor:
+    pos_exp = torch.exp(x)
+    neg_exp = torch.exp(-x)
+    return (pos_exp - neg_exp) / (pos_exp + neg_exp)
+
+
+# Define a baseline network for deep learning
+class BaselineDeepNetwork(nn.Module):
+    def __init__(self, input_size: int = 3072, n_classes: int = 100):
+        super(BaselineDeepNetwork, self).__init__()
+
+        self.input_size = input_size
+        self.n_classes = n_classes
+
+        # -- Layer Definitions -- #
+        # layer 1
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=96, kernel_size=5, stride=1, padding=2
+        )
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=1)
+
+        # layer 2
+        self.conv2 = nn.Conv2d(
+            in_channels=96, out_channels=256, kernel_size=5, stride=2, padding=0
+        )
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=1)
+
+        # layer 3
+        self.conv3 = nn.Conv2d(
+            in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=0
+        )
+        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+
+        # layer 4
+        # After first three layers we're left with [N_examples, 384, 4, 4]
+        self.linear4 = nn.Linear(384 * 4 * 4, 1024)
+
+        # layer 5
+        self.linear5 = nn.Linear(1024, n_classes)
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        # Layer 1
+        x = self.conv1(x)
+        x = ReLU(x)
+        x = self.pool1(x)
+
+        # Layer 2
+        x = self.conv2(x)
+        x = ReLU(x)
+        x = self.pool2(x)
+
+        # Layer 3
+        x = self.conv3(x)
+        x = ReLU(x)
+        x = self.pool3(x)
+
+        # Layer 4
+        x = x.view(x.size(0), -1)  # flatten
+        x = self.linear4(x)
+        x = tanh(x)
+
+        # Layer 5
+        x = self.linear5(x)
+        x = tanh(x)
+
+        return x
+
+    def backward(self, loss: torch.tensor, lr: float = 0.1) -> None:
+        # Reset parameter gradients
+        # NOTE: No learnable parameters for activation functions or pooling layers
+        self.conv1.weight.grad = None
+        self.conv1.bias.grad = None
+        self.conv2.weight.grad = None
+        self.conv2.bias.grad = None
+        self.conv3.weight.grad = None
+        self.conv3.bias.grad = None
+        self.linear4.weight.grad = None
+        self.linear4.bias.grad = None
+        self.linear5.weight.grad = None
+        self.linear5.bias.grad = None
+
+        # Update gradients
+        loss.backward()
+
+        # Update parameters
+        self.conv1.weight.data -= lr * self.conv1.weight.grad
+        self.conv1.bias.data -= lr * self.conv1.bias.grad
+        self.conv2.weight.data -= lr * self.conv2.weight.grad
+        self.conv2.bias.data -= lr * self.conv2.bias.grad
+        self.conv3.weight.data -= lr * self.conv3.weight.grad
+        self.conv3.bias.data -= lr * self.conv3.bias.grad
+        self.linear4.weight.data -= lr * self.linear4.weight.grad
+        self.linear4.bias.data -= lr * self.linear4.bias.grad
+        self.linear5.weight.data -= lr * self.linear5.weight.grad
+        self.linear5.bias.data -= lr * self.linear5.bias.grad
+
+
+# %%
+# Create an instance of the model
+model = BaselineDeepNetwork().to(device)
+
+# Train the model
+model, training_curve = train_model(
+    model=model,
+    train_loader=train_loader,
+    test_loader=test_loader,
+    device=device,
+    lr=0.1,
+    n_epochs=6,
+    print_every=2,
+)
+
+# Save the trained model
+torch.save(model.state_dict(), "baseline_deep_model.pt")
+
+plot_loss_acc(training_curve)
+
+# %%
