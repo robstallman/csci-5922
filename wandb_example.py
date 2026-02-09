a260108d2663da0b592a378509ea50e780f37a15
Rob Stallman <robstallman3@gmail.com>
2026-02-09 10:37:20 -0700

    Add W&B example

---
+++ wandb_example.py
@@ -0,0 +1,35 @@
+import os
+import random
+import wandb
+from dotenv import load_dotenv
+
+load_dotenv()
+wandb.login(key=os.getenv("WAND_API_KEY"))
+
+# Start a new wandb run to track this script.
+run = wandb.init(
+    # Set the wandb entity where your project will be logged (generally your team name).
+    entity="rost5691-cu-boulder",
+    # Set the wandb project where this run will be logged.
+    project="my-awesome-project",
+    # Track hyperparameters and run metadata.
+    config={
+        "learning_rate": 0.02,
+        "architecture": "CNN",
+        "dataset": "CIFAR-100",
+        "epochs": 10,
+    },
+)
+
+# Simulate training.
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2**-epoch - random.random() / epoch - offset
+    loss = 2**-epoch + random.random() / epoch + offset
+
+    # Log metrics to wandb.
+    run.log({"acc": acc, "loss": loss})
+
+# Finish the run and upload any remaining data.
+run.finish()
